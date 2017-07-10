d0dbdfbbc7ab546606b169ba1301b7d33e03bd7a
jianghe <573748150@163.com>
2017-07-10 17:47:05 +0800

    首次提交

---
+++ app.js
@@ -0,0 +1,152 @@
+// 这句的意思就是引入 `express` 模块，并将它赋予 `express` 这个变量等待使用。
+var express = require('express');
+// 调用 express 实例，它是一个函数，不带参数调用时，会返回一个 express 实例，将这个变量赋予 app 变量。
+var app = express();
+
+
+var escapable = /[\\\"\x00-\x1f\x7f-\x9f\u00ad\u0600-\u0604\u070f\u17b4\u17b5\u200c-\u200f\u2028-\u202f\u2060-\u206f\ufeff\ufff0-\uffff]/g,
+    gap,
+    indent,
+    meta = {
+        '\b': '\\b',
+        '\t': '\\t',
+        '\n': '\\n',
+        '\f': '\\f',
+        '\r': '\\r',
+        '"': '\\"',
+        '\\': '\\\\'
+    },
+    rep;
+// json转str方法
+function _json2str(value, replacer, space) {
+    var i;
+    gap = '';
+    indent = '';
+    if (typeof space === 'number') {
+        for (i = 0; i < space; i += 1) {
+            indent += ' ';
+        }
+
+    } else if (typeof space === 'string') {
+        indent = space;
+    }
+    rep = replacer;
+    if (replacer && typeof replacer !== 'function' &&
+        (typeof replacer !== 'object' ||
+        typeof replacer.length !== 'number')) {
+        throw new Error('JSON.stringify');
+    }
+    return str('', {'': value});
+};
+function str(key, holder) {
+    var i,
+        k,
+        v,
+        length,
+        mind = gap,
+        partial,
+        value = holder[key];
+    if (value && typeof value === 'object' &&
+        typeof value.toJSON === 'function') {
+        value = value.toJSON(key);
+    }
+    if (typeof rep === 'function') {
+        value = rep.call(holder, key, value);
+    }
+    switch (typeof value) {
+        case 'string':
+            return quote(value);
+        case 'number':
+            return isFinite(value) ? String(value) : 'null';
+        case 'boolean':
+        case 'null':
+            return String(value);
+        case 'object':
+            if (!value) {
+                return 'null';
+            }
+            gap += indent;
+            partial = [];
+            if (Object.prototype.toString.apply(value) === '[object Array]') {
+                length = value.length;
+                for (i = 0; i < length; i += 1) {
+                    partial[i] = str(i, value) || 'null';
+                }
+                v = partial.length === 0
+                    ? '[]'
+                    : gap
+                        ? '[\n' + gap + partial.join(',\n' + gap) + '\n' + mind + ']'
+                        : '[' + partial.join(',') + ']';
+                gap = mind;
+                return v;
+            }
+            if (rep && typeof rep === 'object') {
+                length = rep.length;
+                for (i = 0; i < length; i += 1) {
+                    if (typeof rep[i] === 'string') {
+                        k = rep[i];
+                        v = str(k, value);
+                        if (v) {
+                            partial.push(quote(k) + (gap ? ': ' : ':') + v);
+                        }
+                    }
+                }
+            } else {
+                for (k in value) {
+                    if (Object.prototype.hasOwnProperty.call(value, k)) {
+                        v = str(k, value);
+                        if (v) {
+                            partial.push(quote(k) + (gap ? ': ' : ':') + v);
+                        }
+                    }
+                }
+            }
+            v = partial.length === 0
+                ? '{}'
+                : gap
+                    ? '{\n' + gap + partial.join(',\n' + gap) + '\n' + mind + '}'
+                    : '{' + partial.join(',') + '}';
+            gap = mind;
+            return v;
+    }
+}
+function quote(string) {
+    escapable.lastIndex = 0;
+    return escapable.test(string) ? '"' + string.replace(escapable, function (a) {
+            var c = meta[a];
+            return typeof c === 'string'
+                ? c
+                : '\\u' + ('0000' + a.charCodeAt(0).toString(16)).slice(-4);
+        }) + '"' : '"' + string + '"';
+}
+
+
+// app 本身有很多方法，其中包括最常用的 get、post、put/patch、delete，在这里我们调用其中的 get 方法，为我们的 `/` 路径指定一个 handler 函数。
+// 这个 handler 函数会接收 req 和 res 两个对象，他们分别是请求的 request 和 response。
+// request 中包含了浏览器传来的各种信息，比如 query 啊，body 啊，headers 啊之类的，都可以通过 req 对象访问到。
+// res 对象，我们一般不从里面取信息，而是通过它来定制我们向浏览器输出的信息，比如 header 信息，比如想要向浏览器输出的内容。这里我们调用了它的 #send 方法，向浏览器输出一个字符串。
+app.get('/', function (req, res) {
+    res.send('测试');
+});
+
+app.get('/getRnVersion', function (req, res) {
+
+    var rnData = {
+        REV: true,
+        msg: {
+            version: 1,//版本号
+            bundleMd5: '666c32e6cd5bb1667bcfb634c807a236',//最新bundleMD5
+            downloadAdd: 'http://192.168.18.77:8080/rnPackger/jj.zip',//增量压缩包的下载地址
+            addMd5: 'aeb568c2b16cda221690069b8df59905',//增量压缩包的MD5
+            downloadAll: 'http://192.168.18.77:8080/rnPackger/jj.zip',//全量压缩包的下载地址
+            allMd5: '666c32e6cd5bb1667bcfb634c807a236',//全量压缩包的MD5
+        }
+    }
+
+    res.send(_json2str(rnData));
+});
+
+// 定义好我们 app 的行为之后，让它监听本地的 3000 端口。这里的第二个函数是个回调函数，会在 listen 动作成功后执行，我们这里执行了一个命令行输出操作，告诉我们监听动作已完成。
+app.listen(3000, function () {
+    console.log('app is listening at port 3000');
+});
